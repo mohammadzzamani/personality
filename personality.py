7910b82e6ececb0649d8c155925cfabd6b56f91e
mzamani <mzamani@cs.stonybrook.edu>
2017-11-22 20:59:29 -0500

    personality.py Util.py

--- personality.py
+++ personality.py
@@ -177,7 +177,7 @@ def load_data():
         raise
     if(cursor is not None):
         # topic_df = load_topics(cursor)
-        topic_df = pd.read_csv('transformed_data.csv')
+        topic_df = pd.read_csv('csv/language.csv')
         # language_df = load_tweets(cursor)
         control_df = load_controls(cursor, control_feats)
         demog_df = load_controls(cursor, demog_feats)
