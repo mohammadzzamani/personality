93d2e95787dff30378b8dfb1d7e4fdb848d721a1
mzamani <mzamani@cs.stonybrook.edu>
2017-12-04 20:17:06 -0500

    county_disease.py

--- county_disease.py
+++ county_disease.py
@@ -130,9 +130,11 @@ def load_ngrams(cursor, index, users=None, ngrams_table = ngrams_table, threshol
     if users is not None:
         ids = '\' , \''.join(users.index.astype(str).values.tolist())
         print (users.iloc[:10].index)
-        sql = "select group_id , feat, value, group_norm from {0} where group_id in ( \'{1}\' )".format(ngrams_table, ids)
+        sql = "select group_id , feat, value, group_norm from {0} where group_id in ( \'{1}\' ) " \
+              "and not feat in (\'!!!!!!!!!!!!!!!!!!!!!!!!\' , \'........................\') ".format(ngrams_table, ids)
     else:
-        sql = "select group_id , feat, value, group_norm from {0} ".format(ngrams_table)
+        sql = "select group_id , feat, value, group_norm from {0} " \
+              "where not feat in (\'!!!!!!!!!!!!!!!!!!!!!!!!\' , \'........................\')  ".format(ngrams_table)
     query = cursor.execute(sql)
     result =  query.fetchall()
     language_df = pd.DataFrame(data = result, columns = [index , 'feat', 'value', 'group_norm'])
